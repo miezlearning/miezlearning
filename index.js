38430ccdbae2aa8b339120b2ddeaaef771c242ed
miezlearning <gottfriedemptiness@gmail.com>
2024-06-23 04:25:49 +0800

    update biar otomatis

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 const Mustache = require("mustache");
 const fs = require("fs");
 const { Octokit } = require("@octokit/rest");
@@ -13,14 +12,13 @@ const octokit = new Octokit({
   },
 });
 
+const githubUsername = process.env.GH_USERNAME;
+
 async function grabDataFromAllRepositories() {
-  // Options under "List repositories for the authenticated user"
-  // https://octokit.github.io/rest.js/v18#authentication
   const options = {
     per_page: 100,
   };
 
-  // https://docs.github.com/en/rest/reference/repos#list-repositories-for-the-authenticated-user
   const request = await octokit.rest.repos.listForAuthenticatedUser(options);
   return request.data;
 }
@@ -44,19 +42,12 @@ async function calculateTotalCommits(data, cutoffDate) {
     const lastRepoUpdate = new Date(repo.updated_at);
 
     if (!cutoffDate || lastRepoUpdate > cutoffDate) {
-      // https://docs.github.com/en/rest/reference/repos#get-all-contributor-commit-activity
       const repoStats = octokit.rest.repos.getContributorsStats(options);
-
       contributorsRequests.push(repoStats);
     }
   });
 
-  const totalCommits = await getTotalCommits(
-    contributorsRequests,
-    githubUsername,
-    cutoffDate
-  );
-
+  const totalCommits = await getTotalCommits(contributorsRequests, githubUsername, cutoffDate);
   return totalCommits;
 }
 
@@ -85,7 +76,6 @@ function computeCommitsFromStart(contributorData) {
 
 function computeCommitsBeforeCutoff(contributorData, cutoffDate) {
   const olderThanCutoffDate = (week) => {
-    // week.w -> Start of the week, given as a Unix timestamp (which is in seconds)
     const MILLISECONDS_IN_A_SECOND = 1000;
     const milliseconds = week.w * MILLISECONDS_IN_A_SECOND;
     const startOfWeek = new Date(milliseconds);
@@ -93,38 +83,25 @@ function computeCommitsBeforeCutoff(contributorData, cutoffDate) {
   };
 
   const newestWeeks = contributorData.weeks.filter(olderThanCutoffDate);
-  // week.c -> Number of commits in a week
   const total = newestWeeks.reduce((sum, week) => sum + week.c, 0);
-
   return total;
 }
 
 async function updateReadme(userData) {
   const TEMPLATE_PATH = "./main.mustache";
-  await fs.readFile(TEMPLATE_PATH, (err, data) => {
-    if (err) {
-      throw err;
-    }
-
-    const output = Mustache.render(data.toString(), userData);
-    fs.writeFileSync("README.md", output);
-  });
+  const data = fs.readFileSync(TEMPLATE_PATH, 'utf8');
+  const output = Mustache.render(data.toString(), userData);
+  fs.writeFileSync("README.md", output);
 }
 
 async function main() {
   const repoData = await grabDataFromAllRepositories();
-
   const totalStars = calculateTotalStars(repoData);
 
   const lastYear = new Date();
   lastYear.setFullYear(lastYear.getFullYear() - 1);
+  const totalCommitsInPastYear = await calculateTotalCommits(repoData, lastYear);
 
-  const totalCommitsInPastYear = await calculateTotalCommits(
-    repoData,
-    lastYear
-  );
-
-  // Hex color codes for the color blocks
   const colors = ["474342", "fbedf6", "c9594d", "f8b9b2", "ae9c9d"];
   await updateReadme({ totalStars, totalCommitsInPastYear, colors });
 }
